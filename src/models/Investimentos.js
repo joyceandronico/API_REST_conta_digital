d3343d073c95336d9f5a50f68e1f829cb7c3e640
Joyce <joyceandronico@msn.com>
2022-07-18 18:36:29 -0300

    feat: :construction: implement schemas (cliente and ativos)

--- src/models/Investimentos.js
+++ src/models/Investimentos.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
-const investSchema = new mongoose.Schema({
-    codAtivo: { type: Number, required: true },
-    qteDisponivelAtivo: { type: Number, required: true },
+const ativo = new mongoose.Schema({
+    codAtivo: { type: Number, required: true, unique: true },
+    qtdeAtivo: { type: Number, required: true },
     valor: { type: Number, required: true },
 });
 
 
-module.exports = mongoose.model('Ativo', investSchema)
+module.exports = mongoose.model('Ativo', ativo);
+
+
 
 
