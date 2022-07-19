fa394eb5e114df2d2206dda1af74e47b5c9244fd
Joyce <joyceandronico@msn.com>
2022-07-18 22:51:25 -0300

    feat: :fire: change db: now using mysql + sequelize

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 require('express-async-errors');
 const bodyParser = require('body-parser');
 const { errorMiddleware } = require('./middlewares/error');
-const mongoose = require('mongoose')
-const url = 'mongodb+srv://joyce_and:ACGK1wrDshiOrBP1@desafioxp.eyycz.mongodb.net/investimentos?retryWrites=true&w=majority'
 const { investRouter } = require('./routes/investimentos.routes');
 const { ativosRouter } = require('./routes/ativos.routes');
 const { contasRouter } = require('./routes/contas.routes');
@@ -17,11 +15,10 @@ app.use('/ativos', ativosRouter);
 app.use('/contas', contasRouter);
 
 
-mongoose.connect(url).then(() => {
-    console.log('conectado ao mongodb!')
-    app.listen(3000);
-})
-    .catch((err) => {
-        app.use(errorMiddleware);
-    })
+
+app.listen(3000, () => {
+    console.log('ouvindo a porta 3000')
+});
+
+app.use(errorMiddleware);
 
--- src/models/Investimentos.js
+++ src/models/Investimentos.js
@@ -1,14 +1,2 @@
-const mongoose = require('mongoose')
-
-const ativo = new mongoose.Schema({
-    codAtivo: { type: Number, required: true, unique: true },
-    qtdeAtivo: { type: Number, required: true },
-    valor: { type: Number, required: true },
-});
-
-
-module.exports = mongoose.model('Ativo', ativo);
-
-
 
 
