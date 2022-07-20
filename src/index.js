4060c9040b05469c68a1e220f91200e448277194
Joyce <joyceandronico@msn.com>
2022-07-20 20:51:04 -0300

    feat: :construction: implementa rotas,  insere dados e conecta db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 require('express-async-errors');
-const bodyParser = require('body-parser');
 const { errorMiddleware } = require('./middlewares/error');
 const { investRouter } = require('./routes/investimentos.routes');
 const { ativosRouter } = require('./routes/ativos.routes');
@@ -8,7 +7,7 @@ const { contasRouter } = require('./routes/contas.routes');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/investimentos', investRouter);
 app.use('/ativos', ativosRouter);
