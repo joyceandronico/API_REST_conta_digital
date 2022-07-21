800f64634fb97eb90775fd52eaa8a956878ca785
Joyce <joyceandronico@msn.com>
2022-07-20 21:57:26 -0300

    feat: :construction: função assetsByCodAtivo - investModel

---
+++ src/models/investModel.js
@@ -0,0 +1,14 @@
+const connection = require('./connection');
+
+const assetsByCodAtivo = async (codAtivo) => {
+    const [data] = await connection.execute(
+        'SELECT * FROM investimentos.ativos WHERE codAtivo = ?',
+        [codAtivo],
+    );
+    const ativo = data[0];
+    return ativo;
+};
+
+module.exports = {
+    assetsByCodAtivo,
+}
